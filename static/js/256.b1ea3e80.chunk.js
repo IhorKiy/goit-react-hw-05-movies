ce61113a9cfaf35c7fa31276270eef4524559e07
IhorKiy <105368656+IhorKiy@users.noreply.github.com>
2023-06-14 12:58:19 +0000

    Deploying to gh-pages from @ IhorKiy/goit-react-hw-05-movies@bd3f0f3713750451e17312e33e9093074f1341b3 🚀

---
+++ static/js/256.b1ea3e80.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{256:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184);function u(){return(0,r.jsx)("p",{children:" 404! Page not found! "})}}}]);
+//# sourceMappingURL=256.b1ea3e80.chunk.js.map
